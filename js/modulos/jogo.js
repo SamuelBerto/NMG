f22ba2c4aefa3d2f7693349007ca6f88d6119a62
Samuel Berto <samuztberto@gmail.com>
2026-05-05 04:15:08 -0300

    feat: implementa base do tabuleiro e virada de cartas

--- js/modulos/jogo.js
+++ js/modulos/jogo.js
@@ -1,1 +1,40 @@
 console.log("Jogo carregado com sucesso!");
+
+const daddos = JSON.parse(localStorage.getItem("jogador"));
+
+if (!daddos) {
+    window.location.href = "index.html";
+
+}
+
+document.getElementById("player").innerText ="Jogador: " + daddos.nome;
+
+const imagens = [
+    "imagens/1.png",
+    "imagens/2.png",
+    "imagens/3.png",
+    "imagens/4.png",
+    "imagens/5.png",
+    "imagens/6.png",
+    
+];
+
+let cartas = [...imagens, ...imagens];
+cartas.sort(() =>0.5 - Math.random());
+
+const tabuleiro =document.getElementById("tabuleiro");
+
+cartas.forEach(img => {
+    const carta = document.createElement("div");
+    carta.classList.add("carta");
+
+    carta.dataset.img = img;
+
+    carta.onclick = () => {
+        carta.innerHTML = `<img src="imagens/${img}">`;
+
+    };
+
+    tabuleiro.appendChild(carta);
+});
+
